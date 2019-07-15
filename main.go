b12020af8a34ee6cc64751ad4747b91516b7dd48
ZadenRB <zaden.ruggieroboune@gmail.com>
2019-07-14 20:40:41 -0600

    Prevents context leak by shutting down webserver context upon closing bot

--- main.go
+++ main.go
@@ -81,8 +81,10 @@ func main() {
 		fmt.Println("error closing Discord session, ", err)
 	}
 
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	if err := server.Shutdown(ctx); err != nil {
 		fmt.Println("error shutting down github webhook,", err)
+	} else {
+		cancel()
 	}
 }
