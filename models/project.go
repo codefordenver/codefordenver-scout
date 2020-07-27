c14ad079249b6f6030b5a4b6f8b4464206adce5c
ZadenRB <zaden.ruggieroboune@gmail.com>
2020-07-27 13:55:42 -0600

    Revert "Update database annotations to set up proper DB"

--- models/project.go
+++ models/project.go
@@ -1,22 +1,16 @@
 package models
 
-import (
-	"database/sql"
-	"github.com/lib/pq"
-)
-
 type Project struct {
-	ID                     int `gorm:"PRIMARY_KEY"`
-	BrigadeID              int `gorm:"type:int REFERENCES brigades(id);NOT NULL;"`
-	Brigade                Brigade
-	Name                   string `gorm:"NOT NULL"`
-	DisplayName            string `gorm:"NOT NULL"`
-	Description            sql.NullString
-	Stack                  sql.NullString
-	HelpNeeded             sql.NullString
-	DiscordChannelID       string         `gorm:"NOT NULL"`
-	GithubDiscordChannelID string         `gorm:"NOT NULL"`
-	DiscordRoleID          string         `gorm:"NOT NULL"`
-	DiscordChampionRoleID  string         `gorm:"NOT NULL"`
-	GithubRepositoryNames  pq.StringArray `gorm:"type:text[];NOT NULL"`
+	ID               int `gorm:"primary_key;AUTO_INCREMENT"`
+	BrigadeID        int `gorm:"not null"`
+	Brigade          Brigade
+	Name             string `gorm:"not null"`
+	Description string
+	Stack string
+	HelpNeeded string
+	DiscordChannelID string `gorm:"not null"`
+	GithubDiscordChannelID string `gorm:"not null"`
+	DiscordRoleID string `gorm:"not null"`
+	DiscordChampionRoleID string `gorm:"not null"`
+	GithubRepository string `gorm:"not null"`
 }
